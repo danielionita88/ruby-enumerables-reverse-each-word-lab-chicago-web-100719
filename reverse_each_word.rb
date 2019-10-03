1a99936dabbb4bd22028576026536b78c92afb3a
Daniel I <danielionita88@yahoo.com>
2019-10-03 18:47:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,16 +1,16 @@
-=begin
 
+=begin
 
 def reverse_each_word(sentence1)
   split_sentence = sentence1.split(" ")
-  reversed_sentence = []
+  reversed_sentence= []
   split_sentence.each {|word| reversed_sentence.push(word.reverse)}
   reversed_sentence.join(" ")
   
 end
 
-
 =end
+
 
 
 
@@ -22,7 +22,6 @@ def reverse_each_word(sentence1)
   
 end
  
-  
   
   
   
